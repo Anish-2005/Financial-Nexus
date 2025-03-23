88c8c1e02e9bd5cdcf59a29be0a5c248a20743f3
Anish-2005 <anishseth0510@gmail.com>
2025-03-24 00:34:28 +0530

    Update FDPage.jsx

--- financer/src/pages/FDPage.jsx
+++ financer/src/pages/FDPage.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
+// Register necessary components
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const FDPage = () => {
